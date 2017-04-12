041e4fc54006dd9eb36168cd1a66b374dfd85e02
Nevan Tan <nevan@tanclan.ca>
2017-04-12 08:31:09 -0400

    Add comments to explain the variables defined in the file

--- environment.example.js
+++ environment.example.js
@@ -1,9 +1,18 @@
+// environment.example.js
+
 module.exports = {
+	// Store any credentials that may be used in tests in this object
 	creds:{
 		admin:{
-			username:"admin",
-			password:"password"
+			username:"admin", // TODO: update "admin" string to admin's username
+			password:"password" // TODO: update "password" string to admin's password
+		},
+		author:{
+			username:"author", // TODO: update "author" string to author's username
+			password:"password" // TODO: update "password" string to author's password
 		}
+		// TODO: add any additional accounts following the pattern above
 	},
-	baseURL:"http://127.0.0.1/test"
+	// All pages accessed in tests will be appended to the baseURL string
+	baseURL:"http://127.0.0.1/test" // TODO: update test site URL
 }
