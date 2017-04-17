a5ba284981954124d2bc73aff7b0c215bd29586d
Nevan Tan <nevan@tanclan.ca>
2017-04-17 09:22:55 -0400

    Add test for iss655

--- ug_event.test.js
+++ ug_event.test.js
@@ -6,6 +6,9 @@ const AddEventPage = require('./pages/node_add_event.js');
 const AdminContentPage = require('./pages/admin_content.js');
 const GenerateTermsPage = require('./pages/admin_config_development_generate_taxonomy.js');
 const AddEventCategoryTermPage = require('./pages/admin_structure_taxonomy_event_category_add.js');
+const EditTermPage = require('./pages/taxonomy_term_edit.js');
+
+import { Selector } from 'testcafe';
 
 fixture `UG Event`
 	.page(Env.baseURL + UserPage.URL)
@@ -18,7 +21,7 @@ fixture `UG Event`
 			// Ensure login successful
 			.expect(UserPage.auth.pageHeader.innerText).eql(Env.creds.admin.username)
 	})
-	/*.afterEach(async t => {
+	.afterEach(async t => {
 		await t
 			.navigateTo(Env.baseURL + AdminContentPage.URL)
 			.click(AdminContentPage.auth.selectAllCheck)
@@ -27,7 +30,7 @@ fixture `UG Event`
 			.click(AdminContentPage.auth.updateButton)
 			.click(AdminContentPage.auth.confirmDelete)
 			.expect("Test").eql("Test");
-	});*/
+	});
 
 test('Test calendar text color', async t => {
 	await t
@@ -43,18 +46,34 @@ test('Test calendar text color', async t => {
 		.expect(EventPage.common.month.grid.allDayEvent.getStyleProperty('color')).eql('rgb(0, 0, 0)');
 });
 
-test('Test Upcoming Events title', async t => {
+test('Descriptive title appears in the correct place', async t => {
 	var startDate = new Date();
 	startDate.setDate(startDate.getDate() + 1);
 
 	var endDate = new Date();
 	endDate.setDate(endDate.getDate() + 2);
 
+	const termName = Util.RandomName(4, 10);
+
 	await t
 		// Create term
 		.navigateTo(Env.baseURL + AddEventCategoryTermPage.URL)
-		.typeText(AddEventCategoryTermPage.common.nameInput, Util.RandomName(4, 10))
-		.click(AddEventCategoryTermPage.common.saveButton)
+		.typeText(AddEventCategoryTermPage.auth.nameInput, termName)
+		.click(AddEventCategoryTermPage.auth.saveButton)
+		// Get TID
+		.navigateTo(Env.baseURL + '/admin/structure/taxonomy/event_category')
+		.click(await Selector('a').withText(termName));
+
+	const tid = await t.eval(function() {
+		var edit = null;
+		document.querySelectorAll('li a').forEach(function(el) {
+			if(el.innerText == "Edit") edit = el;
+		});
+		var arr = /term\/(\d*)\//g.exec(edit.getAttribute('href'));
+		return parseInt(arr[1]);
+	});
+
+	await t
 		// Create node
 		.navigateTo(Env.baseURL + AddEventPage.URL)
 		.typeText(AddEventPage.auth.titleInput, Util.RandomName(5, 12))
@@ -70,5 +89,10 @@ test('Test Upcoming Events title', async t => {
 		// Check page titles
 		.navigateTo(Env.baseURL + EventPage.URL)
 		.expect(EventPage.common.pageHeader.innerText).eql('Upcoming Events')
-		.expect(EventPage.common.pageHeader.innerText).eql('');
+		.navigateTo(Env.baseURL + EventPage.URL + '/' + tid)
+		.expect(EventPage.common.pageHeader.innerText).eql('Events related to ' + termName)
+		// Cleanup term
+		.navigateTo(Env.baseURL + EditTermPage.URL(tid)).wait(500)
+		.click(EditTermPage.auth.deleteButton)
+		.click(EditTermPage.auth.confirmDeleteButton);
 });
