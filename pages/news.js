db567c7f2efc279186adcdb9a7695d2c1d9def24
Nevan Tan <nevan@tanclan.ca>
2017-04-18 12:10:31 -0400

    Restructure pages directory

--- pages/news.js
+++ pages/news.js
@@ -11,13 +11,14 @@ module.exports = {
     panelsAddToLeft:Selector('#panels-ipe-regionid-left').find('a[title="Add new pane"]'),
     panelsAddToRight:Selector('#panels-ipe-regionid-right').find('a[title="Add new pane"]'),
     panelsAddToMiddle:Selector('#panels-ipe-regionid-middle').find('a[title="Add new pane"]'),
-    panelsAddToBottom:Selector('#panels-ipe-regionid-bottom').find('a[title="Add new pane"]'),
+    panelsAddToBottom:Selector('#panels-ipe-regionid-bottom').find('a[title="Add new pane"]')
 	},
 	common:{
-		pageHeader:Selector('h1.page-header'),
+		pageTitle:Selector('h1.page-header'),
     noResults:Selector('div.view-empty'),
     newsListingArea:Selector('div.view-content'),
     newsListingTitle:Selector('h2.media-heading'),
     newsListingTitleLink:Selector('h2.media-heading').find('a'),
+    newsAuthor:Selector('div.media-meta')
 	}
 }
--- pages/news_item.js
+++
@@ -1,8 +0,0 @@
-import { Selector } from 'testcafe';
-
-module.exports = {
-  common:{
-    pageTitle:Selector('h1.page-header'),
-    writtenBy:Selector('div.media-meta')
-  }
-}
