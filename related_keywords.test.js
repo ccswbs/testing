9f7daa939335708abe32aede390a1b8d341003fd
Nevan Tan <nevan@tanclan.ca>
2017-04-28 13:18:15 -0400

    Add test for iss167

--- related_keywords.test.js
+++ related_keywords.test.js
@@ -2,45 +2,79 @@ const Env = require('./environment.js');
 const Util = require('./utils.js');
 const Actions = require('./actions.js');
 const UserPage = require('./pages/user.js');
+const NewsPage = require('./pages/news.js');
+const EventPage = require('./pages/events.js');
 const PeoplePage = require('./pages/people.js');
-const AddProfilePage = require('./pages/node/add/profile.js');
-const AdminContentPage = require('./pages/admin/content.js');
-const TaxonomyTermPage = require('./pages/taxonomy/term/edit.js');
 import { Selector } from 'testcafe';
 
 fixture `UG Profile`
 	.page(Env.baseURL + UserPage.URL)
 	.beforeEach(async t => {
+		
+	});
+
+test
+	.before(async t => {
+		// Actions.CreateNode returns a node ID that is then
+		// stored in t.ctx.[type]_nid to be referenced later
+		// for testing and deletion
+		
+		var tags = [];
+		for(var i = 0; i < 5; i++) {
+			tags.push(Util.RandomName(4, 10));
+		}
+		tags = tags.toString();
+
+		// Create News
+		t.ctx.news_nid = await Actions.CreateNode({
+			type:"news",
+			title:"Test news",
+			tags:tags
+		});
+		// Create Event
+		t.ctx.event_nid = await Actions.CreateNode({
+			type:"event",
+			title:Util.RandomName(4, 10),
+			tags:tags
+		});
+		// Create Profile
+		t.ctx.profile_nid = await Actions.CreateNode({
+			type:"profile",
+			name:{
+				first:Util.RandomName(4, 10),
+				last:Util.RandomName(4, 10)
+			},
+			tags:tags
+		});
+		// Create FAQ
+		t.ctx.faq_nid = await Actions.CreateNode({
+			type:"faq",
+			question:"Test Question",
+			answer:{
+				value:"Test Answer"
+			},
+			tags:tags
+		});
+	})('Related keywords on E6, F4, PP4, and N7 are consistent', async t => {
+		var pattern = /^(\w*,\s*)*\w*$/g; // Regex to match comma
+
 		await t
-			// Authenticate
-			.typeText(UserPage.anon.userInput, Env.creds.admin.username)
-			.typeText(UserPage.anon.passInput, Env.creds.admin.password)
-			.click(UserPage.anon.submitButton)
-			// Ensure login successful
-			.expect(UserPage.auth.pageHeader.innerText).eql(Env.creds.admin.username)
+			// Test News related keywords for pattern
+			.navigateTo(Env.baseURL + '/node/' + await t.ctx.news_nid)
+			.expect(NewsPage.common.relatedKeywords.innerText).match(pattern)
+			// Test Event related keywords for pattern
+			.navigateTo(Env.baseURL + '/node/' + await t.ctx.event_nid)
+			.expect(EventPage.common.relatedKeywords.innerText).match(pattern)
+			// Test Profile related keywords for pattern
+			.navigateTo(Env.baseURL + '/node/' + await t.ctx.profile_nid)
+			.expect(PeoplePage.common.relatedKeywords.innerText).match(pattern)
+			// Test FAQ related keywords for pattern
+			//.navigateTo(Env.baseURL + '/node/' + await t.ctx.faq_nid)
+			//.expect(FAQPage)
 	})
-	/*.afterEach(async t => {
-		await t
-			.navigateTo(Env.baseURL + AdminContentPage.URL)
-			.click(AdminContentPage.auth.selectAllCheck)
-			.click(AdminContentPage.auth.operationSelect)
-			.click(AdminContentPage.auth.operationDeleteOption)
-			.click(AdminContentPage.auth.updateButton)
-			.click(AdminContentPage.auth.confirmDelete)
-			.expect("Test").eql("Test");
-	});*/
-
-test('Related keywords on E6, F4, PP4, and N7 are consistent', async t => {
-	const name = Util.RandomName(4, 10);
-	const lastname = Util.RandomName(4, 10);
-	const email = Util.RandomEmail();
-	const phone = Util.RandomPhone('519-824-4120 ext. #####');
-	const pattern = false; //;
-
-	const term = Util.RandomName(4, 10);
-	var tid = await Actions.CreateTerm(Util.Vocabulary.tags, term);
-
-	await t
-		.navigateTo(Env.baseURL + TaxonomyTermPage.URL(tid))
-		.expect(TaxonomyTermPage.auth.pageTitle.innerText).eql(term);
-});
+	.after(async t => {
+		await Actions.DeleteNode(t.ctx.event_nid);
+		await Actions.DeleteNode(t.ctx.profile_nid);
+		await Actions.DeleteNode(t.ctx.faq_nid);
+		await Actions.DeleteNode(t.ctx.news_nid);
+	});
