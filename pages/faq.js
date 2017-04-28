3a43ddece1e472befa1944a605dc76a97676a7d8
Nevan Tan <nevan@tanclan.ca>
2017-04-28 13:15:03 -0400

    Add definition for faq page

---
+++ pages/faq.js
@@ -0,0 +1,15 @@
+import { Selector } from 'testcafe';
+
+module.exports = {
+	URL:'/faq',
+	anon:{
+		errorMessage:Selector('.alert')
+	},
+	auth:{
+		
+	},
+	common:{
+		pageHeader:Selector('h1.page-header'),
+		relatedKeywords:Selector('.view-f4 .view-content')
+	}
+};
