96d99a246dc02c46b00725dfd4945d3bcf9617ca
Nevan Tan <nevan@tanclan.ca>
2017-05-03 09:16:44 -0400

    Rework events tests WIP

--- ug_event.test.js
+++ ug_event.test.js
@@ -1,5 +1,6 @@
 const Env = require('./environment.js');
 const Util = require('./utils.js');
+const Actions = require('./actions.js');
 const UserPage = require('./pages/user.js');
 const EventPage = require('./pages/events.js');
 const AddEventPage = require('./pages/node/add/event.js');
@@ -11,88 +12,33 @@ const EditTermPage = require('./pages/taxonomy/term/edit.js');
 import { Selector } from 'testcafe';
 
 fixture `UG Event`
-	.page(Env.baseURL + UserPage.URL)
-	.beforeEach(async t => {
-		await t
-			// Authenticate
-			.typeText(UserPage.anon.userInput, Env.creds.admin.username)
-			.typeText(UserPage.anon.passInput, Env.creds.admin.password)
-			.click(UserPage.anon.submitButton)
-			// Ensure login successful
-			.expect(UserPage.auth.pageHeader.innerText).eql(Env.creds.admin.username)
-	})
-	.afterEach(async t => {
-		await t
-			.navigateTo(Env.baseURL + AdminContentPage.URL)
-			.click(AdminContentPage.auth.selectAllCheck)
-			.click(AdminContentPage.auth.operationSelect)
-			.click(AdminContentPage.auth.operationDeleteOption)
-			.click(AdminContentPage.auth.updateButton)
-			.click(AdminContentPage.auth.confirmDelete)
-			.expect("Test").eql("Test");
-	});
+	.page(Env.baseURL);
 
-test('Test calendar text color', async t => {
-	await t
-		// Create node
-		.navigateTo(Env.baseURL + AddEventPage.URL)
-		.typeText(AddEventPage.auth.titleInput, 'Test Event')
-		.click(AddEventPage.auth.allDayCheck)
-		.click(AddEventPage.auth.saveButton)
-		// Navigate to calendar page
-		.navigateTo(Env.baseURL + EventPage.URL + '/month/grid')
-		.expect(EventPage.common.pageHeader.innerText).eql('Monthly Calendar')
-		.expect(EventPage.common.month.grid.allDayEvent.innerText).contains('Test Event')
-		.expect(EventPage.common.month.grid.allDayEvent.getStyleProperty('color')).eql('rgb(0, 0, 0)');
-});
+test
+	.before(async t => {
+		// Dummy Content
+		t.ctx.catname = Util.RandomName(4, 10);
+		t.ctx.eventtitle = Util.RandomName(4, 10);
 
-test('Descriptive title appears in the correct place', async t => {
-	var startDate = new Date();
-	startDate.setDate(startDate.getDate() + 1);
-
-	var endDate = new Date();
-	endDate.setDate(endDate.getDate() + 2);
-
-	const termName = Util.RandomName(4, 10);
-
-	await t
-		// Create term
-		.navigateTo(Env.baseURL + AddEventCategoryTermPage.URL)
-		.typeText(AddEventCategoryTermPage.auth.nameInput, termName)
-		.click(AddEventCategoryTermPage.auth.saveButton)
-		// Get TID
-		.navigateTo(Env.baseURL + '/admin/structure/taxonomy/event_category')
-		.click(await Selector('a').withText(termName));
-
-	const tid = await t.eval(function() {
-		var edit = null;
-		document.querySelectorAll('li a').forEach(function(el) {
-			if(el.innerText == "Edit") edit = el;
+		// Create Event Category
+		t.ctx.tid = await Actions.CreateTerm(Util.Vocabulary['event_category'], t.ctx.catname, "");
+		// Create Event Node
+		t.ctx.event = await Actions.CreateNode({
+			type:"event",
+			title:t.ctx.eventtitle,
+			tid:t.ctx.tid
 		});
-		var arr = /term\/(\d*)\//g.exec(edit.getAttribute('href'));
-		return parseInt(arr[1]);
+	})('Descriptive title appears in the correct place', async t => {
+		await t
+			// Check page titles
+			.navigateTo(Env.baseURL + EventPage.URL)
+			.expect(EventPage.common.pageHeader.innerText).eql('Upcoming Events')
+			.navigateTo(Env.baseURL + EventPage.URL + '/' + t.ctx.tid)
+			.expect(EventPage.common.pageHeader.innerText).eql('Events related to ' + t.ctx.catname)
+			.expect(await Selector('h2.media-heading').exists).ok()
+			.expect(await Selector('h3.media-heading').exists).notOk();
+	})
+	.after(async t => {
+		await Actions.DeleteTerm(t.ctx.tid);
+		await Actions.DeleteNode(t.ctx.event);
 	});
-
-	await t
-		// Create node
-		.navigateTo(Env.baseURL + AddEventPage.URL)
-		.typeText(AddEventPage.auth.titleInput, Util.RandomName(5, 12))
-		.click(AddEventPage.auth.categorySelect)
-		.click(AddEventPage.auth.categoryFirstOption)
-		.selectText(AddEventPage.auth.startDateInput)
-		.pressKey('backspace')
-		.typeText(AddEventPage.auth.startDateInput, Util.FormatDate(startDate, 'M j Y'))
-		.selectText(AddEventPage.auth.endDateInput)
-		.pressKey('backspace')
-		.typeText(AddEventPage.auth.endDateInput, Util.FormatDate(endDate, 'M j Y'))
-		.click(AddEventPage.auth.saveButton)
-		// Check page titles
-		.navigateTo(Env.baseURL + EventPage.URL)
-		.expect(EventPage.common.pageHeader.innerText).eql('Upcoming Events')
-		.navigateTo(Env.baseURL + EventPage.URL + '/' + tid)
-		.expect(EventPage.common.pageHeader.innerText).eql('Events related to ' + termName)
-		// Cleanup term
-		.navigateTo(Env.baseURL + EditTermPage.URL(tid)).wait(500)
-		.click(EditTermPage.auth.deleteButton)
-		.click(EditTermPage.auth.confirmDeleteButton);
-});
