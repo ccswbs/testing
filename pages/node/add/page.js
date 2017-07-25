ca6741e2f828f0cd2ae00ac2f132d8d3112d0f34
Nevan Tan <nevan@tanclan.ca>
2017-07-25 14:58:57 -0400

    Change add to menu check target from checkbox to label

--- pages/node/add/page.js
+++ pages/node/add/page.js
@@ -4,7 +4,7 @@ module.exports = {
 	URL:'/node/add/profile',
 	auth:{
 		pageTitle:Selector('h1.page-title'),
-		enableMenuCheck:Selector('#edit-menu-enabled', {visibilityCheck:true}),
+		enableMenuCheck:Selector('label', {visibilityCheck:true}).withAttribute('for', 'edit-menu-enabled'),
 		menuParentSelect:Selector('#edit-menu-parent', {visibilityCheck:true}),
 		saveButton:Selector('#edit-submit', {visibilityCheck:true}),
 		deleteButton:Selector('#edit-delete', {visibilityCheck:true}),
